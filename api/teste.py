b3d8b027aa0679dff99dacef5fdb2e295d5d25b1
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2024-03-01 08:44:45 -0300

    Update teste.py

--- api/teste.py
+++ api/teste.py
@@ -4,7 +4,7 @@ import requests
 class TestAPI(unittest.TestCase):
     def test_call_api_multiple_times(self):
         url = 'http://127.0.0.1:5000/km/jundiai/guarulhos'  # Substitua pela sua URL
-        for _ in range(10):
+        for _ in range(30):
             response = requests.get(url)
             self.assertEqual(response.status_code, 200)  # Verifica se a chamada foi bem-sucedida
             print(response.json())  # Imprime o JSON retornado pela API
